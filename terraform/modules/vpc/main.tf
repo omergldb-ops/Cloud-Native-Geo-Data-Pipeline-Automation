2fdcecf4dd1cf1d9a7d92c44c34b058f6006891d
omer g <omergldb@gmail.com>
2026-01-04 16:06:49 +0200

    vpc  subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,14 +1,22 @@
 resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
 }
+
 resource "aws_subnet" "private" {
-  count             = 2
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "10.0.${count.index}.0/24"
-  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+  count = length(var.private_subnet_cidrs) > 0 ? length(var.private_subnet_cidrs) : var.private_subnet_count
+
+  vpc_id = aws_vpc.this.id
+
+  cidr_block = length(var.private_subnet_cidrs) > 0 ? var.private_subnet_cidrs[count.index] : cidrsubnet(aws_vpc.this.cidr_block, 8, count.index)
+
+  availability_zone = element(var.availability_zones, count.index)
+
   tags = {
     Name = "private-subnet-${count.index}"
   }
 }
-resource "aws_internet_gateway" "igw" { vpc_id = aws_vpc.this.id }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+}
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,4 +1,23 @@
 variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+}
+
+variable "private_subnet_count" {
+  type        = number
+  default     = 2
+  description = "Number of private subnets to create when `private_subnet_cidrs` is not provided"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  default     = []
+  description = "Optional list of CIDR blocks for private subnets. If non-empty, these will be used instead of auto-calculated CIDRs."
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
+  description = "Availability zones to place subnets in; length should match private subnet count when providing explicit cidrs."
 }
