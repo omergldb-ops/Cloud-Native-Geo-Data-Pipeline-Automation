965d7edd44653af71ad0542b09e05ab4e9f2897e
omer g <omergldb@gmail.com>
2026-01-04 16:01:30 +0200

    vpc  subnets

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,8 +1,9 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
+  description = "ID of the VPC"
+  value       = aws_vpc.this.id
 }
-  output "private_subnets"
-  {
-  value = aws_subnet.private[*].id
+
+output "private_subnets" {
   description = "List of IDs of private subnets"
-  }
+  value       = aws_subnet.private[*].id
+}
