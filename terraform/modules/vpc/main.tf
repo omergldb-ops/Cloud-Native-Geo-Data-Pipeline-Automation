0d208ffa1c342de2eec250d3f81030a883441d00
omer g <omergldb@gmail.com>
2026-01-04 15:59:14 +0200

    vpc  subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -6,6 +6,9 @@ resource "aws_subnet" "private" {
   count             = 2
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.0.${count.index}.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
 }
 resource "aws_internet_gateway" "igw" { vpc_id = aws_vpc.this.id }
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,11 +1,8 @@
 output "vpc_id" {
   value = aws_vpc.this.id
 }
-
-output "private_subnets" {
-  value = [
-    aws_subnet.private_1.id, 
-    aws_subnet.private_2.id
-  ]
+  output "private_subnets"
+  {
+  value = aws_subnet.private[*].id
   description = "List of IDs of private subnets"
-}
+  }
