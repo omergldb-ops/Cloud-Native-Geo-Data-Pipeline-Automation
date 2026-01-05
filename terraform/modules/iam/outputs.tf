e42b2eed3758f477ac87c43fe9ba6baa7b89bf0e
omer g <omergldb@gmail.com>
2026-01-05 10:10:20 +0200

    roles outputs

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,3 +1,4 @@
-output "role_arn" {
-  value = aws_iam_role.app_role.arn
+output "app_role_arn" {
+  value       = aws_iam_role.app_role.arn
+  description = "The ARN of the IAM role for the application"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
-output "ecr_url" {
-  value = module.storage.ecr_url
+output "app_role_arn" {
+  value = module.iam.app_role_arn
 }
 
-output "s3_bucket" {
-  value = module.storage.s3_bucket_name
+output "ecr_url" {
+  value = module.ecr.repository_url # וודא שזה השם אצלך
 }
 
 output "rds_endpoint" {
-  value = module.db.db_endpoint
+  value = module.db.db_instance_endpoint # וודא שזה השם אצלך
 }
 
-output "app_iam_role_arn" {
-  value = module.iam.role_arn
+output "s3_bucket" {
+  value = module.s3.bucket_name # וודא שזה השם אצלך
 }
