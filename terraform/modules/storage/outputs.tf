b329610c709c8b3a7f0c26a48ab2f3f07633a916
omer g <omergldb@gmail.com>
2026-01-05 10:38:15 +0200

    outputs changed

--- terraform/modules/storage/outputs.tf
+++ terraform/modules/storage/outputs.tf
@@ -1,7 +1,7 @@
-output "ecr_url" {
+output "ecr_repository_url" {
   value = aws_ecr_repository.app_repo.repository_url
 }
 
 output "s3_bucket_name" {
-  value = aws_s3_bucket.data_bucket.id
+  value = aws_s3_bucket.app_data.id
 }
