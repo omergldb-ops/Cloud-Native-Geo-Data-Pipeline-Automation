d7b993fc47e40c2aebc1a55ed376863433d863db
omer g <omergldb@gmail.com>
2026-01-05 10:26:26 +0200

    outputs changed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,27 @@
+# תיקון ה-ECR: גישה למשאב הנכון ששלחת קודם
+output "ecr_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
+
+# תיקון ה-EKS: גישה דרך המודול (module.eks)
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "oidc_provider" {
+  value = module.eks.oidc_provider
+}
+
+# ה-Role של האפליקציה מהמודול של ה-IAM
 output "app_role_arn" {
   value = module.iam.app_role_arn
 }
 
-output "ecr_url" {
-  value = eks.repository_url
-}
-
+# (אופציונלי) כתובת ה-RDS וה-S3 כדי שה-Workflow יוכל למשוך אותם
 output "rds_endpoint" {
-  value = module.db.db_endpoint 
+  value = module.db.db_instance_endpoint 
 }
 
 output "s3_bucket" {
-  value = module.storage.s3_bucket_name 
+  value = module.s3.bucket_name
 }
